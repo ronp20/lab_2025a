b4cb1c03d8e6418bbb1b73928a8c942f7bdf249a
ronp20 <52355612+ronp20@users.noreply.github.com>
2024-10-15 21:58:07 +0300

    letters

--- Mmn_11/letters/letters.c
+++ Mmn_11/letters/letters.c
@@ -1,58 +1,92 @@
+/*
+ * Created By Ron Potashnik 2024.
+ */
+
 #include <stdio.h>
 #include <ctype.h>
+#include <unistd.h>  /* Required for isatty() */
 
 typedef int BOOL;  /* Define BOOL as an alias for int */
 #define TRUE 1
 #define FALSE 0
 
-int main() 
+int main()
 {
     char c;
     BOOL new_sentence = TRUE;  /* Track if we are at the start of a new sentence */
     BOOL inside_quotes = FALSE; /* Track if we are inside quotes */
 
-    /* Loop to read input until EOF */
-    while ((c = getchar()) != EOF) 
+    /* Check if the input is coming from a terminal (no redirection) */
+    if (isatty(fileno(stdin)))
     {
-        if (c == '"') 
+        printf("Hello, Please enter the text. The program will process your input:\n When you are done, press Ctrl+D(Linux) or Ctrl+Z(Windows) to exit.\n\n");
+    }
+    else
+    {
+        printf("Processing input...\n\n");
+    }
+
+    /* Loop to read input until EOF */
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '"')
         {
             /* Toggle the state for quotes explicitly */
-            if (inside_quotes == TRUE) 
+            if (inside_quotes == TRUE)
             {
                 inside_quotes = FALSE;  /* Now outside quotes */
-            } 
-            else 
+            }
+            else
             {
                 inside_quotes = TRUE;   /* Now inside quotes */
             }
+
+            if (new_sentence == TRUE)
+            {
+                new_sentence = FALSE;  /* Reset the new sentence flag */
+            }
+
             putchar(c);  /* Print the quote character */
-        } 
-        else if (new_sentence && isalpha(c)) 
+        }
+        else if (inside_quotes && isalpha(c))
+        {
+            putchar(toupper(c));  /* Inside quotes, convert to uppercase */
+        }
+        else if (new_sentence && isalpha(c))
         {
             putchar(toupper(c));  /* Convert to uppercase at the start of a sentence */
             new_sentence = FALSE;  /* Reset the new sentence flag */
-        } 
-        else if (inside_quotes && isalpha(c)) 
-        {
-            putchar(toupper(c));  /* Inside quotes, convert to uppercase */
-        } 
-        else if (!inside_quotes && isalpha(c)) 
+        }
+        else if (!inside_quotes && isalpha(c))
         {
             putchar(tolower(c));  /* Convert to lowercase outside of quotes */
-        } 
-        else if (isdigit(c)) 
+        }
+        else if (isdigit(c))
         {
+            if (new_sentence == TRUE)
+            {
+                new_sentence = FALSE;  /* Reset the new sentence flag */
+            }
+
             continue;  /* Ignore digits */
-        } 
-        else 
+        }
+        else
         {
             putchar(c);  /* Print other characters as-is */
-            if (c == '.' || c == '!' || c == '?') 
+
+            if (c != ' ' && new_sentence == TRUE)
+            {
+                new_sentence = FALSE;  /* Reset the new sentence flag */
+            }
+
+            /* End of sentence handling */
+            if (c == '.' && inside_quotes == FALSE)
             {
                 new_sentence = TRUE;  /* Mark the start of a new sentence */
             }
         }
     }
 
+    putchar('\n');  /* Print a newline at the end */
     return 0;
 }
